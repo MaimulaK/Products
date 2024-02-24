a26bc5d0dedb6dc921bfee056e26641f2268a7bf
MaimulaK <maimulakonstantin@gmail.com>
2024-02-24 12:47:49 +0300

    release 3

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server has been started at port ${PORT}`);
+});
---
+++ components/Layout.jsx
@@ -0,0 +1,14 @@
+// components/Layout.jsx
+const React = require('react');
+module.exports = function Layout({ title, children }) {
+  return (
+    <html lang='en'>
+      <head>
+        <title>{title}</title>
+        <link rel='stylesheet' href='/css/style.css' />
+        <script src='/js/script.js' />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+};
