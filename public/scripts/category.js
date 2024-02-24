b4c1ef73fdd343313c035604eba7cb08f62c7925
MaimulaK <maimulakonstantin@gmail.com>
2024-02-25 02:30:14 +0300

    release notwork

---
+++ public/scripts/category.js
@@ -0,0 +1,26 @@
+const categoryAdd = document.querySelector(".add-form");
+// const cardContainer = document.querySelector(".card-container");
+
+if (categoryAdd) {
+  categoryAdd.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const { name, img } = event.target;
+    const data = {
+      nameCategory: name.value,
+      img: img.value,
+    };
+    const response = await fetch("/api/categories", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const res = await response.json();
+    console.log(res);
+    if (res.message === "success") {
+      categoryAdd.reset();
+      document
+        .querySelector(".card-container")
+        .insertAdjacentHTML("beforeend", res.html);
+    }
+  });
+}
