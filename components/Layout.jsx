afca430b24cae5a48f9f79085809320a66fc0d5c
MaimulaK <maimulakonstantin@gmail.com>
2024-02-25 11:07:42 +0300

    release materialize

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,8 +6,15 @@ module.exports = function Layout({ title, children }) {
     <html lang='en'>
       <head>
         <title>{title}</title>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel='stylesheet' href='/styles/index.css' />
-        {/* <script src='/js/script.js' /> */}
+        <link type="text/css" rel="stylesheet" href="public/styles/materialize.min.css"  media="screen,projection"/>
+        <script
+          defer
+          src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
+        />
+        <script defer type="text/javascript" src="public/scripts/materialize.min.js"></script>
         <script defer src='/scripts/category.js' />
         
       </head>
